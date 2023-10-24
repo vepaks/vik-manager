1d6a19262f3d3ddeba4695eb045471d97e22c377
Aleksandar Kamburov <kamburoff@gmail.com>
2023-10-24 16:31:49 +0300

    add express config

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log('Dev')
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+const PORT = config.get("serverPort");
+const dbUrl = config.get("DB_URL");
+
+const start = () => {
+  try {
+      mongoose.connect(`${dbUrl}`);
+    app.listen(PORT, () => {
+      console.log(`server is running http://localhost:${PORT}..`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
