52a3ac9dfeaa26c7b659c50aa7ee899d07f918ed
Aleksandar Kamburov <kamburoff@gmail.com>
2023-10-27 13:17:30 +0300

    add redux, userReducer, fileReducer,

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,17 @@ import React, {StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import {store} from "./reducers"
+import {Provider} from "react-redux";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <StrictMode>
+    <Provider store={store}>
+        <StrictMode>
             <App />
-    </StrictMode>,
+        </StrictMode>,
+    </Provider>
+
 );
