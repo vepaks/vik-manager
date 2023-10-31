e87a6400ee8f98bc2c79ef2eaa3fdf5a6f1c5118
Aleksandar Kamburov <kamburoff@gmail.com>
2023-10-31 13:20:58 +0200

    add table widget

--- client/src/widgets/Table/ui/Table.jsx
+++ client/src/widgets/Table/ui/Table.jsx
@@ -4,11 +4,9 @@ import cls from "./Table.module.scss"
 
 
 export const Table = ({className}) => {
-
-
     return (
-        <div className={classNames(cls.Table, {}, [className])}>
-            <h1 >
+        <div className={classNames(cls.Table, {}, [className, 'text-3xl cursor-not-allowed'])}>
+            <h1 className={classNames(cls, {}, [className, 'text-3xl cursor-not-allowed'])} >
                Table
             </h1>
         </div>
