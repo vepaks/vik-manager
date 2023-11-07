5f1dad75a0afd0410c9a7dc1ab3c15cc75316928
Aleksandar Kamburov <kamburoff@gmail.com>
2023-11-07 13:18:44 +0200

    add file logic

--- client/src/pages/auth/LoginPage/ui/LoginPage.jsx
+++ client/src/pages/auth/LoginPage/ui/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import {LoginForm} from "../../../../features/auth";
 
 const LoginPage = () => {
--- client/src/widgets/Tables/FileTable/ui/FileTable.jsx
+++ client/src/widgets/Tables/FileTable/ui/FileTable.jsx
@@ -1,40 +1,33 @@
 import {classNames} from "../../../../shared/lib/classNames/classNames";
 import cls from './FileTable.module.scss'
+// import {useSelector} from "react-redux";
+import {File} from "../File";
 
 export const FileTable = ({className}) => {
+
+    // const files = useSelector(state => state.files.files).map( file => <File/> )
+    const files = [
+        {_id: 1, name: 'Документи за блок 14', type: 'dir', size: "3gb", date: '11.11.2023'},
+        {_id: 2, name: 'Документи за блок 8', type: 'photo', size: "1gb", date: '08.11.2023'},
+    ].map(file => <File key={file._id}/> )
     return (
         <div className={classNames(cls.Table, {}, [className])}>
             <div className={classNames(cls.table, {}, [className])}>
 
                 <div className={classNames(cls.row, {}, [className, "header"])}>
                     <div className={classNames(cls.cell, {}, [className, "row"])}>
-                        Име и Фамилия
+                    </div>
+                    <div className={classNames(cls.cell, {}, [className, "row"])}>
+                      Название
                     </div>
                     <div className={classNames(cls.cell, {}, [className])}>
-                        Адрес
+                        Дата
                     </div>
                     <div className={classNames(cls.cell, {}, [className])}>
-                        Стойност
-                    </div>
-                    <div className={classNames(cls.cell, {}, [className])}>
-                        Действие
+                     Размер
                     </div>
                 </div>
-
-                <div className={classNames(cls.row, {}, [className])}>
-                    <div className={classNames(cls.cell, {}, [className])} data-title="Name">
-                        Иван Георгиев
-                    </div>
-                    <div className={classNames(cls.cell, {}, [className])} data-title="Adress">
-                        гр.София
-                    </div>
-                    <div className={classNames(cls.cell, {}, [className])} data-title="Value">
-                        102585
-                    </div>
-                    <div className={classNames(cls.cell, {}, [className])} data-title="Action">
-                        Виж
-                    </div>
-                </div>
+                {files}
             </div>
         </div>
     );
