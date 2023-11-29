8e0ae3d4316a921dc2d113b9952fc101b94412ab
Aleksandar Kamburov <kamburoff@gmail.com>
2023-11-29 19:52:32 +0200

    add hook for registration

--- client/src/features/auth/models/registration.jsx
+++ client/src/features/auth/models/registration.jsx
@@ -1,52 +1,30 @@
-import {setError} from "../../../app/reducers/errorReducer";
+import { useDispatch } from "react-redux";
 import axios from "axios";
-import {useDispatch} from "react-redux";
+import { setError } from "../../../app/reducers/errorReducer";
 
-export const registration = async (email, password) => {
+const useRegistration = () => {
+  const dispatch = useDispatch();
 
+  const registration = async (email, password) => {
     try {
-        const response = await axios.post("http://localhost:5000/api/auth/signup", {
-            email,
-            password,
-        });
-        console.log(response.status);
-        console.log(response.data.message);
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/signup",
+        {
+          email,
+          password,
+        },
+      );
+
+      console.log(response.status);
+      console.log(response.data.message);
     } catch (e) {
-        console.log(e.response.status);
-        console.log(e.response.data.message);
+      console.log(e.response.status);
+      console.log(e.response.data.message);
+      dispatch(setError(e.response.data.message));
     }
+  };
+
+  return { registration };
 };
 
-//TODO make a custom Hook registration
-
-
-//// registrationHook.js
-// import { useDispatch } from 'react-redux';
-// import axios from 'axios';
-// import { setError } from '../../../app/reducers/errorReducer';
-//
-// const useRegistration = () => {
-//   const dispatch = useDispatch();
-//
-//   const registration = async (email, password) => {
-//     try {
-//       const response = await axios.post('http://localhost:5000/api/auth/signup', {
-//         email,
-//         password,
-//       });
-//
-//       console.log(response.status);
-//       console.log(response.data.message);
-//     } catch (e) {
-//       console.log(e.response.status);
-//       console.log(e.response.data.message);
-//
-//       // Dispatch the setError action to update the error state
-//       dispatch(setError(e.response.data.message));
-//     }
-//   };
-//
-//   return { registration };
-// };
-//
-// export default useRegistration;
+export default useRegistration;
--- client/src/features/auth/ui/SignupForm/ui/SignupForm.jsx
+++ client/src/features/auth/ui/SignupForm/ui/SignupForm.jsx
@@ -3,13 +3,14 @@ import cls from "./SignupForm.module.scss";
 import { Input } from "../../../../../shared/ui/Input";
 import { Button } from "../../../../../shared/ui/Button";
 import React, { useState } from "react";
-import {registration} from "../../../models/registration";
+import useRegistration from "../../../models/registration";
 
 export const SignupForm = (props, { className }) => {
   // const [FirstName, setFirstName] = useState("");
   // const [LastName, setLastName] = useState("");
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
+  const { registration } = useRegistration();
 
   return (
     <div className={classNames(cls, {}, [className])}>
@@ -33,100 +34,93 @@ export const SignupForm = (props, { className }) => {
               "w-full px-6 py-4 mt-6 overflow-hidden shadow-md sm:max-w-md sm:rounded-lg",
             ])}
           >
-              {/*<div className="mt-4">*/}
-              {/*  <label className="block text-m font-medium text-gray-700 undefined">*/}
-              {/*    Име*/}
-              {/*  </label>*/}
-              {/*  <div className="flex flex-col items-start ">*/}
-              {/*    <Input*/}
-              {/*      value={FirstName}*/}
-              {/*      type="text"*/}
-              {/*      setValue={setFirstName}*/}
-              {/*      placeholder="въведете име"*/}
-              {/*    />*/}
-              {/*  </div>*/}
-              {/*</div>*/}
-              {/*<div className="mt-4">*/}
-              {/*  <label className="block text-m font-medium text-gray-700 undefined">*/}
-              {/*    Фамилия*/}
-              {/*  </label>*/}
-              {/*  <div className="flex flex-col items-start">*/}
-              {/*    <Input*/}
-              {/*      value={LastName}*/}
-              {/*      setValue={setLastName}*/}
-              {/*      type="text"*/}
-              {/*      placeholder="въведете фамилия"*/}
-              {/*    />*/}
-              {/*  </div>*/}
-              {/*</div>*/}
-              <div className="mt-4">
-                <label className="block text-m font-medium text-gray-700 undefined">
-                  Email
-                </label>
-                <div className="flex flex-col items-start">
-                  <Input
-                    value={Email}
-                    setValue={setEmail}
-                    type="email"
-                    placeholder="въведете email"
-                  />
-                </div>
+            {/*<div className="mt-4">*/}
+            {/*  <label className="block text-m font-medium text-gray-700 undefined">*/}
+            {/*    Име*/}
+            {/*  </label>*/}
+            {/*  <div className="flex flex-col items-start ">*/}
+            {/*    <Input*/}
+            {/*      value={FirstName}*/}
+            {/*      type="text"*/}
+            {/*      setValue={setFirstName}*/}
+            {/*      placeholder="въведете име"*/}
+            {/*    />*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+            {/*<div className="mt-4">*/}
+            {/*  <label className="block text-m font-medium text-gray-700 undefined">*/}
+            {/*    Фамилия*/}
+            {/*  </label>*/}
+            {/*  <div className="flex flex-col items-start">*/}
+            {/*    <Input*/}
+            {/*      value={LastName}*/}
+            {/*      setValue={setLastName}*/}
+            {/*      type="text"*/}
+            {/*      placeholder="въведете фамилия"*/}
+            {/*    />*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+            <div className="mt-4">
+              <label className="block text-m font-medium text-gray-700 undefined">
+                Email
+              </label>
+              <div className="flex flex-col items-start">
+                <Input
+                  value={Email}
+                  setValue={setEmail}
+                  type="email"
+                  placeholder="въведете email"
+                />
               </div>
-              <div className="mt-4">
-                <label className="block text-m font-medium text-gray-700 undefined">
-                  Password
-                </label>
-                <div className="flex flex-col items-start">
-                  <Input
-                    value={Password}
-                    setValue={setPassword}
-                    type="password"
-                    placeholder="въведете парола"
-                  />
-                </div>
+            </div>
+            <div className="mt-4">
+              <label className="block text-m font-medium text-gray-700 undefined">
+                Password
+              </label>
+              <div className="flex flex-col items-start">
+                <Input
+                  value={Password}
+                  setValue={setPassword}
+                  type="password"
+                  placeholder="въведете парола"
+                />
               </div>
-              {/*<div className="mt-4">*/}
-              {/*    <label*/}
-              {/*        htmlFor="password_confirmation"*/}
-              {/*        className="block text-sm font-medium text-gray-700 undefined"*/}
-              {/*    >*/}
-              {/*        Confirm Password*/}
-              {/*    </label>*/}
-              {/*    <div className="flex flex-col items-start">*/}
-              {/*        <input*/}
-              {/*            type="password"*/}
-              {/*            name="password_confirmation"*/}
-              {/*            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"*/}
-              {/*        />*/}
-              {/*    </div>*/}
-              {/*</div>*/}
-              <div className="flex items-center justify-end mt-4">
-                <a
-                  className="text-sm text-gray-600 underline hover:text-gray-900"
-                  href="/login"
-                >
-                  Вече имаш регистрация?
-                </a>
-                <Button
-                  className={classNames(cls.Button, {}, [
-                    "inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md active:bg-gray-900 false",
-                  ])}
-                  type="submit"
-                  onClick={() =>
-                    registration(Email, Password)
-                  }
-                >
-                  Регистрация
-                </Button>
-              </div>
+            </div>
+            {/*<div className="mt-4">*/}
+            {/*    <label*/}
+            {/*        htmlFor="password_confirmation"*/}
+            {/*        className="block text-sm font-medium text-gray-700 undefined"*/}
+            {/*    >*/}
+            {/*        Confirm Password*/}
+            {/*    </label>*/}
+            {/*    <div className="flex flex-col items-start">*/}
+            {/*        <input*/}
+            {/*            type="password"*/}
+            {/*            name="password_confirmation"*/}
+            {/*            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"*/}
+            {/*        />*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <div className="flex items-center justify-end mt-4">
+              <a
+                className="text-sm text-gray-600 underline hover:text-gray-900"
+                href="/login"
+              >
+                Вече имаш регистрация?
+              </a>
+              <Button
+                className={classNames(cls.Button, {}, [
+                  "inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md active:bg-gray-900 false",
+                ])}
+                type="submit"
+                onClick={() => registration(Email, Password)}
+              >
+                Регистрация
+              </Button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-
-//TODO make a useRegistration Hook
-
-// const { registration } = useRegistration();
