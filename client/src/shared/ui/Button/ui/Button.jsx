4f1b313bb4ef79a218a311290fcbc366209f47fb
Aleksandar Kamburov <kamburoff@gmail.com>
2023-12-01 22:50:06 +0200

    add translate of file table

--- client/src/shared/ui/Button/ui/Button.jsx
+++ client/src/shared/ui/Button/ui/Button.jsx
@@ -28,4 +28,3 @@ export const Button = (props) => {
         </button>
     );
 };
-
--- client/src/widgets/Tables/FileTable/ui/FileTable.jsx
+++ client/src/widgets/Tables/FileTable/ui/FileTable.jsx
@@ -2,24 +2,25 @@ import {classNames} from "../../../../shared/lib/classNames/classNames";
 import cls from './FileTable.module.scss'
 import {useSelector} from "react-redux";
 import {File} from "../File";
+import {useTranslation} from "react-i18next";
 
 export const FileTable = ({className}) => {
 
     const files = useSelector(state => state.files.files)
-
+    const {t} = useTranslation()
     return (
         <div className={classNames(cls.Table, {}, [className])}>
                 <div className={classNames(cls.header, {}, [className])}>
                     <div className={classNames(cls, {}, [className])}>
                     </div>
                     <div className={classNames(cls.name, {}, [className])}>
-                     Документация
+                        {t("Документация")}
                     </div>
                     <div className={classNames(cls.date, {}, [className])}>
-                        Дата
+                        {t("Дата")}
                     </div>
                     <div className={classNames(cls.size, {}, [className])}>
-                     Размер
+                        {t("Размер")}
                     </div>
                 </div>
             { files && files.map(file => <File key={file.id} file={file}/>)}
