adc7e6c9016fefd0718e742ebf74f59cab1f7ac9
Aleksandar Kamburov <kamburoff@gmail.com>
2023-12-11 20:18:41 +0200

    update registration form

--- client/src/features/auth/models/registration.jsx
+++ client/src/features/auth/models/registration.jsx
@@ -1,29 +1,34 @@
-import { useDispatch } from "react-redux";
+// src/features/auth/models/registration.jsx
 import axios from "axios";
+import store from "../../../app/reducers/userReducer";
 import { setError } from "../../../app/reducers/errorReducer";
 
 const useRegistration = () => {
-  const dispatch = useDispatch();
+    const dispatch = store.dispatch;
 
-  const registration = async (email, password) => {
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/api/auth/signup",
-        {
-          email,
-          password,
-        },
-      );
-      console.log(response.status);
-      console.log(response.data.message);
-    } catch (e) {
-      console.log(e.response.status);
-      console.log(e.response.data.message);
-      dispatch(setError(e.response.data.message));
-    }
-  };
+    const registration = async (email, password) => {
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/api/auth/signup",
+                {
+                    email,
+                    password,
+                }
+            );
+            console.log(response.status);
+            console.log(response.data.message);
+            return response;
+        } catch (e) {
+            console.log(e.response.status);
+            console.log(e.response.data.message);
+            dispatch(
+                setError(e.response ? e.response.data.message : "Unknown error")
+            );
+            throw e; // Rethrow the error to propagate it to the caller
+        }
+    };
 
-  return { registration };
+    return { registration };
 };
 
 export default useRegistration;
