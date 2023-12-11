7898bf878ff560912231fb31f76a56028e4ba591
Aleksandar Kamburov <kamburoff@gmail.com>
2023-12-11 10:49:59 +0200

    update details table

--- client/src/widgets/Tables/DetailTable/Details/ui/Details.jsx
+++ client/src/widgets/Tables/DetailTable/Details/ui/Details.jsx
@@ -1,25 +1,18 @@
-import {classNames} from "../../../../../shared/lib/classNames/classNames";
-import cls from './Details.module.scss'
-import {Button} from "../../../../../shared/ui/Button";
+import cls from './Details.module.scss';
+import { Button } from "../../../../../shared/ui/Button";
 import dataLogo from "../../../../../shared/assets/icons/dataLogo.svg";
 
+export const Details = ({ details = {} }) => {
+  const { address, date, data = [] } = details;
+  const lastDataItem = data.length > 0 ? data[data.length - 1] : null;
 
-export const Details = ({details}) => {
-    return (
-        <div className={classNames(cls.Details, {}, [])}>
-            <img src={dataLogo} alt="logo" className={classNames(cls.img, {}, [])}/>
-            <div >
-                {details.address}
-            </div>
-            <div className={classNames(cls.date, {}, [])}>
-                {details.date}
-            </div>
-            <div className={classNames(cls.size, {}, [])}>
-                {details.data[details.data.length - 1]}
-            </div>
-
-            <Button  className={classNames(cls.delete, {}, [])} >Изтрий</Button>
-        </div>
+  return (
+    <div className={cls.Details}>
+      <img src={dataLogo} alt="logo" className={cls.img} />
+        {address && <div>{address}</div>}
+        {date && <div className={cls.date}>{date}</div>}
+        {lastDataItem && <div className={cls.size}>{lastDataItem}</div>}
+        <Button className={cls.delete} >Изтрий</Button>
+    </div>
     );
 };
-
