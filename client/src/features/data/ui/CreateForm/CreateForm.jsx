a150b8f6796bcd07a1e4dc079c97c400deafb036
Aleksandar Kamburov <kamburoff@gmail.com>
2023-12-11 09:52:41 +0200

    add validations to create form

--- client/src/features/data/ui/CreateForm/CreateForm.jsx
+++ client/src/features/data/ui/CreateForm/CreateForm.jsx
@@ -18,10 +18,32 @@ export const CreateForm = ({ className }) => {
   const [additionalData, setAdditionalData] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const parentId = useSelector(state => state.user.currentUser?.id);
-  console.log(parentId)
 
+  // Validation states
+  const [addressError, setAddressError] = useState("");
+  const [chatIdError, setUserIdError] = useState("");
+  const [additionalDataError, setAdditionalDataError] = useState("");
 
   const handleClick = async () => {
+    // Reset errors
+    setAddressError("");
+    setUserIdError("");
+    setAdditionalDataError("");
+
+    // Add validation here
+    if (address.trim() === "") {
+      setAddressError(t("Адресата е задължителен!"));
+      return;
+    }
+    if (chatId.trim() === "") {
+      setUserIdError(t("Номерът на потребителя е задължителен!"));
+      return;
+    }
+    if (additionalData.trim() === "") {
+      setAdditionalDataError(t("Полето за данни е задължително!"));
+      return;
+    }
+
     try {
       await postData(chatId, parentId, address, additionalData);
       setSuccessMessage(t("Добавихте нови данни! Добави още?"));
@@ -59,16 +81,9 @@ export const CreateForm = ({ className }) => {
                   </p>
                 </div>
             )}
-            <div
-              className={classNames(cls.CreateForm, {}, [
-                className,
-                "w-full px-6 py-4 mt-6 overflow-hidden shadow-md sm:max-w-md sm:rounded-lg",
-              ])}
-            >
+            <div	className={classNames(cls.CreateForm, {}, [	className, "w-full px-6 py-4 mt-6 overflow-hidden shadow-md sm:max-w-md sm:rounded-lg",])}>
               <div className="mt-4">
-                <label className="block text-m font-medium text-gray-700 undefined">
-                  {t("Адрес на подребителя")}
-                </label>
+                <label className="block text-m font-medium text-gray-700 undefined">{t("Адрес на подребителя")}</label>
                 <div className="flex flex-col items-start">
                   <Input
                     type="text"
@@ -76,12 +91,11 @@ export const CreateForm = ({ className }) => {
                     setValue={setAddress}
                     placeholder={t("въведете адрес")}
                   />
+                  { addressError && <span className="text-red-500">{addressError}</span>}
                 </div>
               </div>
               <div className="mt-4">
-                <label className="block text-m font-medium text-gray-700 undefined">
-                  {t("Номер на потребителя")}
-                </label>
+                <label className="block text-m font-medium text-gray-700 undefined">{t("Номер на потребителя")}</label>
                 <div className="flex flex-col istems-start">
                   <Input
                     type="text"
@@ -89,25 +103,25 @@ export const CreateForm = ({ className }) => {
                     setValue={setUserId}
                     placeholder={t("въведете номер")}
                   />
+                  { chatIdError && <span className="text-red-500">{chatIdError}</span>}
                 </div>
               </div>
               <div className="mt-4">
-                <label className="block text-m font-medium text-gray-700 undefined">
-                  {t("Добавете данни")}
-                </label>
+                <label className="block text-m font-medium text-gray-700 undefined">{t("Добавете данни")}</label>
                 <div className="flex flex-col istems-start">
                   <Input
-                    type="text"
-                    value={additionalData}
-                    setValue={setAdditionalData}
-                    placeholder={t("добавете данни")}
+                   type="text"
+                   value={additionalData}
+                   setValue={setAdditionalData}
+                   placeholder={t("добавете данни")}
                   />
+                  { additionalDataError && <span className="text-red-500">{additionalDataError}</span>}
                 </div>
               </div>
               <div className="flex items-center justify-end mt-4">
                 <Button
                   className={classNames(cls.Button, {}, [
-                    "inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md active:bg-gray-900 false text-center",
+                  "inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md active:bg-gray-900 false text-center",
                   ])}
                   type="submit"
                   onClick={handleClick}
